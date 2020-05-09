7f07a9f0981a1b8e6ea5625563a643b3866d303b
Duy <cunbidun@gmail.com>
2020-05-09 12:38:13 +0700

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello World!</div>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
