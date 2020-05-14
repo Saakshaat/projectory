aefd3c6c4a41422eff010f997d83d2acf2d059c4
Duy <cunbidun@gmail.com>
2020-05-14 20:41:36 +0700

    change color

--- client/src/Utils/Skill.js
+++ client/src/Utils/Skill.js
@@ -2,10 +2,10 @@
 module.exports = [
   { name: "C", color: "#84D9A7" },
   { name: "C++", color: "#1FA34B" },
-  { name: "Java", color: "#F8B739" },
+  { name: "Java", color: "#F2924F" },
   { name: "Node.js", color: "#7A8A33" },
   { name: "Python", color: "#6DF26E " },
-  { name: "React", color: "#8034CC" },
+  { name: "React", color: "#F03654" },
   { name: "Docker", color: "#9F628D" },
   { name: "Kubernetes", color: "#F28096" },
   { name: "Google Cloud Platform", color: "#515C1C" },
